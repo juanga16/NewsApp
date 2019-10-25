8f335192f842cecd1ad89f16c1f9674aabbeb8a1
Juanga <juanga141280@gmail.com>
2019-10-25 10:25:41 -0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Cosme Fulanito on 25/10/2019.
+//  Copyright © 2019 Cosme Fulanito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
