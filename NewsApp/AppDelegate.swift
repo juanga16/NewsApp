9232d1b432c595d28efba12cfe64f4c4ca030d5b
Juanga <juanga141280@gmail.com>
2019-10-25 11:15:36 -0700

    App starts without Storyboard

--- NewsApp/AppDelegate.swift
+++ NewsApp/AppDelegate.swift
@@ -17,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let initialController = InitialController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = initialController
+        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
---
+++ NewsApp/Controllers/InitialController.swift
@@ -0,0 +1,26 @@
+//
+//  InitialController.swift
+//  NewsApp
+//
+//  Created by Cosme Fulanito on 25/10/2019.
+//  Copyright © 2019 Cosme Fulanito. All rights reserved.
+//
+
+import UIKit
+
+class InitialController: UIViewController {
+    
+    override func viewDidLoad() {
+        view = UIView()
+        view.backgroundColor = .blue
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello there"
+        label.textColor = .white
+        
+        view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
